60d7262de7a13b38f5ee527f9b999605a501ff03
Toby Rufinus <tobyrufinus@gmail.com>
2017-01-03 21:03:55 +0100

    Remove old code

--- main.scala
+++ main.scala
@@ -35,29 +35,6 @@ object Main extends IO {
       loop(hash(text, m), hash(pattern, m), 0)
     }
 
-    // NODE: This function is now outdated
-    def matchRabinKarpNoFold(s: String, p: String): Int = {
-      def hash(str: String): Int = {
-        var h = 0
-        for (char <- str) {
-          h = h + char
-        }
-        h
-      }
-      val n = s.length
-      val m = p.length
-      val pHash = hash(p)
-      for (i <- 0 to n - m) {
-        val sHash = hash(s.substring(i, i + m))
-        if (sHash == pHash) {
-          if (s.substring(i, i + m) == p) {
-            return i
-          }
-        }
-      }
-      return -1
-    }
-
     val predefinedString = "Since Fender Stratocaster is a classic guitar, Scalacaster is about classic algorithms and data structures in Scala. Scalacaster includes loads of widely used implementation techniques and approaches, which have been developed by best programmers and enthusiasts of functional programming. Studying purely functional data structures is always fun and challenge for researchers, since data structures in a functional setting are much elegant and smarter than in an imperative setting."
 
     val patterns = List(
