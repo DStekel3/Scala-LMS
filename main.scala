8198497dfe96e8918a3f741fbeacfeec7d392289
Toby Rufinus <tobyrufinus@gmail.com>
2017-01-04 13:12:37 +0100

    Add (unfinished) staged version of matchRabinKarp

--- main.scala
+++ main.scala
@@ -15,8 +15,8 @@ object Main extends IO {
      * Space - O(1)
      */
     def matchRabinKarp(text: String, pattern: String): Int = {
-      val n = text.length()
-      val m = pattern.length()
+      val n = text.length
+      val m = pattern.length
       val q = 3355439 // a large prime
       val a = 256     // the base of the rolling hash
       val aMax = (1 until m).foldLeft(1)((res, i) => mod(res * a, q)) // aMax = a^(m-1) % q 
@@ -35,6 +35,37 @@ object Main extends IO {
       loop(hash(text, m), hash(pattern, m), 0)
     }
 
+    var snippet = new DslDriver[String,Int] {
+      def snippet(str: Rep[String]) = {
+
+        def matchRabinKarpStaged(text: Rep[String], pattern: String): Rep[Int] = {
+          val n = text.length
+          val m = pattern.length
+          val q = 3355439 // a large prime
+          val a = 256     // the base of the rolling hash
+          val aMax = (1 until m).foldLeft(1)((res, i) => mod(res * a, q)) // aMax = a^(m-1) % q 
+
+          def hash(string: String, length: Int): Int = 
+            (0 until length).foldLeft(0)((hash, i) => mod(hash * a + string.charAt(i), q)) 
+
+          def hashStaged(string: Rep[String], length: Int): Rep[Int] = 
+            (0 until length).foldLeft(0)((hash, i) => mod(hash * a + string.charAt(i), q)) 
+
+          def loop(textHash: Rep[Int], patternHash: Int, i: Int): Rep[Int] = {
+            if (textHash == patternHash) i
+            else if (i == n - m) -1
+            else {
+              val tempHash = mod(textHash - mod(aMax * text.charAt(i), q), q)     // subtract old character
+              loop(mod(tempHash * a + text.charAt(i + m), q), patternHash, i + 1) // add new character and loop
+            }
+          }
+          loop(hashStaged(text, m), hash(pattern, m), 0)
+        }
+        matchRabinKarpStaged(str, predefinedPattern)
+      }
+    }
+
+    val predefinedPattern = "Scala"
     val predefinedString = "Since Fender Stratocaster is a classic guitar, Scalacaster is about classic algorithms and data structures in Scala. Scalacaster includes loads of widely used implementation techniques and approaches, which have been developed by best programmers and enthusiasts of functional programming. Studying purely functional data structures is always fun and challenge for researchers, since data structures in a functional setting are much elegant and smarter than in an imperative setting."
 
     val patterns = List(
