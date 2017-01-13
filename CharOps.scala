d1e767236bffa696179eaa969190240a7d4246ba
Toby Rufinus <tobyrufinus@gmail.com>
2017-01-13 13:39:49 +0100

    Add implicit conversion of Rep[Char] to Rep[Int] - New traits are defined in CharOps.Scala - These are mixed in in dslapi.scala so DslDriver works with the new staged operations - The staged Rabin-Karp snippet is disabled; a simple test snippet adding staged chars and ints is available

---
+++ CharOps.scala
@@ -0,0 +1,31 @@
+import scala.lms.common._
+
+trait CharOps extends Base with PrimitiveOps {
+  implicit def repCharToRepInt(x: Rep[Char]): Rep[Int] = x.toInt
+  //def infix_+(x: Rep[Int], y: Rep[Char]): Rep[Int] = int_plus(x, repCharToRepInt(y))
+
+  implicit def charToCharOps(c: Char): CharOpsCls = new CharOpsCls(unit(c))
+  implicit def repCharToCharOps(c: Rep[Char]): CharOpsCls = new CharOpsCls(c)
+  implicit def varCharToCharOps(c: Var[Char]): CharOpsCls = new CharOpsCls(readVar(c))
+
+  class CharOpsCls(c: Rep[Char]) {
+    def toInt: Rep[Int] = char_to_int(c)
+  }
+  def char_to_int(c: Rep[Char]): Rep[Int]
+}
+
+trait CharExp extends CharOps with BaseExp with PrimitiveOpsExp {
+  case class CharToInt(c: Exp[Char]) extends Def[Int]
+
+  def char_to_int(c: Exp[Char]) = CharToInt(c)
+}
+
+trait ScalaGenCharOps extends ScalaGenBase {
+  val IR: CharExp
+  import IR._
+  
+  override def emitNode(sym: Sym[Any], node: Def[Any]) = node match {
+    case CharToInt(c) => emitValDef(sym, quote(c) + ".toInt")
+    case _ => super.emitNode(sym, node)
+  }
+}
--- main.scala
+++ main.scala
@@ -1,8 +1,11 @@
 import scala.lms.common._
-import Math.{floorMod => mod}
 
 object Main extends IO {
   val under = "Strings"
+
+  
+  val predefinedPattern = "Scala"
+  val predefinedString = "Since Fender Stratocaster is a classic guitar, Scalacaster is about classic algorithms and data structures in Scala. Scalacaster includes loads of widely used implementation techniques and approaches, which have been developed by best programmers and enthusiasts of functional programming. Studying purely functional data structures is always fun and challenge for researchers, since data structures in a functional setting are much elegant and smarter than in an imperative setting."
 
   def main(args: Array[String]): Unit = {
     /**
@@ -15,6 +18,10 @@ object Main extends IO {
      * Space - O(1)
      */
     def matchRabinKarp(text: String, pattern: String): Int = {
+      // Scala's % operator can return negative numbers;
+      // the floorMod() function guarantees nonnegative results
+      import Math.{floorMod => mod}
+
       val n = text.length
       val m = pattern.length
       val q = 3355439 // a large prime
@@ -46,53 +53,69 @@ object Main extends IO {
       loop(hash(text, m), hash(pattern, m), 0)
     }
 
-    var snippet = new DslDriver[String,Int] {
-      def snippet(str: Rep[String]) = {
+    // var snippet = new DslDriver[String,Int] {
+    //   def snippet(str: Rep[String]) = {
 
-        def matchRabinKarpStaged(text: Rep[String], pattern: String): Rep[Int] = {
-          val n:Rep[Int] = text.length
-          val m:Int = pattern.length
-          val q:Int = 3355439 // a large prime
-          val a:Int = 256     // the base of the rolling hash
-          val aMax:Int = {    // aMax = a^(m-1) % q 
-            var res: Int = 1
-            for (i <- 1 until m) {
-              res = mod(res * a, q)
-            }
-            res
-          }
-          def hash(string: String, length: Int): Int = {
-            var hash:Int = 0
-            for (i <- 0 until length) {
-              hash = mod(hash * a /*+ string.charAt(i)*/, q)
-            }
-            hash
-          }
+    //     def matchRabinKarpStaged(text: Rep[String], pattern: String): Rep[Int] = {
+    //       // Scala's % operator can return negative numbers;
+    //       // the mod() function guarantees nonnegative results
+    //       def mod(x: Rep[Int], y: Int): Rep[Int] = {
+    //         var res: Rep[Int] = x % y
+    //         if (x < 0)
+    //           res += y
+    //         res
+    //       }
 
-          // def hashStaged(string: Rep[String], length: Int): Rep[Int] = {
-          //   var hash = 0
-          //   for (i <- 0 until length) {
-          //     hash = mod(hash * a + string.charAt(i), q)
-          //   }
-          //   hash
-          // }
+    //       val n: Rep[Int] = text.length
+    //       val m: Int = pattern.length
+    //       val q: Int = 3355439 // a large prime
+    //       val a: Int = 256     // the base of the rolling hash
+    //       val aMax: Int = {    // aMax = a^(m-1) % q 
+    //         var res: Int = 1
+    //         for (i <- 1 until m) {
+    //           res = mod(res * a, q)
+    //         }
+    //         res
+    //       }
 
-          def loop(textHash: Rep[Int], patternHash: Int, i: Int): Rep[Int] = {
-            if (textHash == patternHash) i
-            else if (i == n - m) -1
-            else {
-              val tempHash = mod(textHash - mod(aMax /* * text.charAt(i)*/, q), q)     // subtract old character
-              loop(mod(tempHash * a /*+ text.charAt(i + m)*/, q), patternHash, i + 1) // add new character and loop
-            }
-          }
-          loop(/*hashPattern(text, m)*/0, hash(pattern, m), 0)
-        }
-        matchRabinKarpStaged(str, predefinedPattern)
+    //       def hash(string: String, length: Int): Int = {
+    //         var hash:Int = 0
+    //         for (i <- 0 until length) {
+    //           hash = mod(hash * a + string.charAt(i), q)
+    //         }
+    //         hash
+    //       }
+
+    //       // def hashStaged(string: Rep[String], length: Int): Rep[Int] = {
+    //       //   var hash = 0
+    //       //   for (i <- 0 until length) {
+    //       //     hash = mod(hash * a + string.charAt(i), q)
+    //       //   }
+    //       //   hash
+    //       // }
+
+    //       def loop(textHash: Rep[Int], patternHash: Int, i: Int): Rep[Int] = {
+    //         if (textHash == patternHash) i
+    //         else if (i == n - m) -1
+    //         else {
+    //           val tempHash = mod(textHash - mod(aMax /* * text.charAt(i)*/, q), q)     // subtract old character
+    //           loop(mod(tempHash * a /*+ text.charAt(i + m)*/, q), patternHash, i + 1) // add new character and loop
+    //         }
+    //       }
+    //       loop(/*hashPattern(text, m)*/0, hash(pattern, m), 0)
+    //     }
+    //     matchRabinKarpStaged(str, predefinedPattern)
+    //   }
+    // }
+
+    var simpleSnippet = new DslDriver[Char,Int] {
+      def snippet(c: Rep[Char]) = {
+        val someInteger: Int = 10
+        c + someInteger + c
       }
     }
 
-    val predefinedPattern = "Scala"
-    val predefinedString = "Since Fender Stratocaster is a classic guitar, Scalacaster is about classic algorithms and data structures in Scala. Scalacaster includes loads of widely used implementation techniques and approaches, which have been developed by best programmers and enthusiasts of functional programming. Studying purely functional data structures is always fun and challenge for researchers, since data structures in a functional setting are much elegant and smarter than in an imperative setting."
+    println(simpleSnippet.eval('A'))
 
     val patterns = List(
       "Scala",
@@ -104,11 +127,13 @@ object Main extends IO {
       "perative setting!"
     )
 
-    val patternSearchResults = patterns.map(string => matchRabinKarp(predefinedString, string))
-    val combinedPatternSearchResults = patterns zip patternSearchResults
-    for ((string, result) <- combinedPatternSearchResults) {
-      println(s"$string -> $result")
-    }
+    //val patternSearchResults = patterns.map(string => matchRabinKarp(predefinedString, string))
+    //val patternSearchResults = patterns.map(string => simpleSnippet.eval(predefinedString))
+
+    // val combinedPatternSearchResults = patterns zip patternSearchResults
+    // for ((string, result) <- combinedPatternSearchResults) {
+    //   println(s"$string -> $result")
+    // }
   }
 }
 
