a7f68bd59d623e8062668c55891bd68aba66fc4f
Toby Rufinus <tobyrufinus@gmail.com>
2017-01-17 18:53:49 +0100

    Code cleanup

--- main.scala
+++ main.scala
@@ -1,53 +1,18 @@
-object Main {
+import scala.lms.common._
+
+object Main extends IO {
+  val under = "Example"
   
   def main(args: Array[String]): Unit = {
-
-    // The Getting Started tutorial - see start.scala
-    // https://scala-lms.github.io/tutorials/start.html 
-    // println("\n::: Getting started :::")
-    // GettingStarted.run()
-
-    // // The Shonan HMM Challenge tutorial - see shonan.scala
-    // // https://scala-lms.github.io/tutorials/shonan.html
-    // println("\n::: Shonan HMM Challenge :::")
-    // Shonan.run()
-
-    // // The Regular Expressions tutorial - see regex.scala
-    // // https://scala-lms.github.io/tutorials/regex.html
-    // println("\n::: Regular Expressions :::")
-    // Regex.run()
-
-    // // The Automata-Based Regex Matcher tutorial - see automata.scala
-    // // https://scala-lms.github.io/tutorials/automata.html
-    // println("\n::: Automata-Based Regex Matcher :::")
-    // Automata.run()
-
-    // // The Sliding Stencil tutorial - see stencil.scala
-    // // https://scala-lms.github.io/tutorials/stencil.html
-    // println("\n::: Sliding Stencil :::")
-    // Stencil.run()
-
-    // println("\n::: Searching methods :::")
-    // Search.run();
-
-    /* Running this test along with the others may result in Java GC/out of memory errors
-     * If it is run by itself, with the other tests disabled, it works fine */
-    // The Ackermann's Function tutorial - see ack.scala
-    // https://scala-lms.github.io/tutorials/ack.html
-    // println("\n::: Ackermann's Function :::")
-    // Ackermann.run()
-
-    /* The following tutorials are not included:
-
-     * The SQL Engine tutorial
-     * https://scala-lms.github.io/tutorials/query.html
-     * Far more code than the other tutorials, spread over multiple files
-
-     * The Fast Fourier tutorial
-     * https://scala-lms.github.io/tutorials/fft.html
-     * The code, as it appears on the site, was sloppily copied from a paper
-     * It is completely broken
-
-     */
+    val snippet = new DslDriver[String, Unit] {
+      def snippet(str: Rep[String]) = {
+        println("Hello, " + str)
+      }
+    }
+    println("Generating code snippet in /out/" + under +"-hello.actual.scala")
+    exec("-hello", snippet.code)
+    
+    println("Running code snippet")
+    snippet.eval("world!")
   }
 }
