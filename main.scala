8d556747070e53201a88f59eb19cfe6e939b51c8
Daniël Stekelenburg <d.stekelenbug@afas.nl>
2017-01-27 13:05:42 +0100

    testing mergetool

--- main.scala
+++ main.scala
@@ -1,7 +1,7 @@
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Running Rabin-Karp string search algorithm...")
+    //println("Running Rabin-Karp string search algorithm...")
     RabinKarp.run()
     println("\nRunning binary tree search algorithm...")
     TreeSearch.run();
